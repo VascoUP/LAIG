b857651887f640bbb5c29668cb9f08692d915534
saracouto1318 <saracouto1318@hotmail.com>
2016-10-23 15:59:13 +0100

    More ifs

--- TP1/WebCGF.XMLReaderExample/reader/MySceneGraph.js
+++ TP1/WebCGF.XMLReaderExample/reader/MySceneGraph.js
@@ -172,6 +172,9 @@ MySceneGraph.prototype.parseScenes = function(scene_elems) {
 	if(this.graph.idHead == 'undefined' || this.axis_length == 'undefined')
 		return "Scene -> Missing required information.";
 	
+	else if(this.axis_length < 0)
+		console.warn("Axis length can't be negative");
+	
 };
 
 //Parses the the different views' elements
@@ -203,9 +206,6 @@ MySceneGraph.prototype.parseViews = function(views_elems) {
 		
 		if(id == 'undefined' || near == 'undefined' || far == 'undefined' || angle == 'undefined')
 			return ("Views -> Prespective " + id + " -> Missing required information.");
-		
-		if(angle < 0 || angle > 2*Math.PI)
-			return "Views -> Prespective " + id + " -> Angle with wrong values.";
 
 		//Making sure that there are no to prespectives with the same id
 		for(var j = 0; j < this.views.length; j++) {
@@ -447,7 +447,7 @@ MySceneGraph.prototype.parseLights = function(lights) {
 			var angle = Math.PI * this.reader.getFloat(light, 'angle') / 180 ;
 			var exponent = this.reader.getFloat(light, 'exponent') ;
 			
-			if(( 0 > angle || angle > 2 * Math.PI || angle == 'undefined') || exponent == 'undefined')
+			if(angle == 'undefined' || exponent == 'undefined')
 				return "Lights -> Spot -> Missing required information or variables with wrong values.";
 			
 			this.scene.lights[this.nLights].setSpotCutOff( angle );
@@ -492,18 +492,34 @@ MySceneGraph.prototype.parseTextures = function(texture) {
 
 	var texture_elem = texture.getElementsByTagName('texture');
 	
+	if(texture_elem.length < 1)
+		return "Texture -> You need at least one texture.";
+	
 	for( var i = 0; i < texture_elem.length; i++ ) {
 		var textureElem = texture_elem[i];
 		var id = this.reader.getString(texture_elem[i], 'id');
+		
+		if( id == 'undefined' )
+			return "Texture -> ID -> Missing the required information."
 
 		// Making sure that there are no two textures with the same id 
 		if( this.textures[id] != undefined)
 			return "Textures-> There are 2 textures with the same id (id="+id+")";
 
 		var file = this.reader.getString(texture_elem[i], 'file');
+		
+		if( file == 'undefined' )
+			return "Texture -> Missing required information -> " + id + " -> File undefined";
+		
 		var t = new CGFtexture(this.scene, file);
-		var texInfo = new TextureInfo( t, this.reader.getFloat(texture_elem[i], 'length_t'), 
-											this.reader.getFloat(texture_elem[i], 'length_s'))
+		
+		var length_t = this.reader.getFloat(texture_elem[i], 'length_t');
+		var length_s = this.reader.getFloat(texture_elem[i], 'length_s');
+		
+		if( length_t == 'undefined' || length_s == 'undefined')
+			return "Texture -> Missing required information -> " + id + " -> length_t or length_s undefined";
+		
+		var texInfo = new TextureInfo( t, length_t, length_s);
 
 		this.textures[id] = texInfo;
 	}
@@ -512,12 +528,6 @@ MySceneGraph.prototype.parseTextures = function(texture) {
 //Parses the materials
 MySceneGraph.prototype.parseMaterials = function(material) {
 	
-	/*
-		Materials
-
-		TO DO:
-			????????????????
-	*/
 	var nnodes = material.children.length;
 	
 	if(material == null || nnodes < 1) 
@@ -533,10 +543,13 @@ MySceneGraph.prototype.parseMaterials = function(material) {
 			return "Materials -> Material error";
 
 		var id = material_elems.attributes.getNamedItem('id').value;
+		
+		if( id == 'undefined')
+			return "Materials -> Material -> ID -> Missing required information.";
 	
 		// Making sure that there are no two materials with the same id 
 		if( this.materials[id] != undefined )
-			return "Materials -> Same id error";
+			return "Materials -> " + this.materials[id] + " -> Same id error";
 
 		var appearance = new CGFappearance(this.scene);
 
@@ -552,6 +565,12 @@ MySceneGraph.prototype.parseMaterials = function(material) {
 		b = this.reader.getFloat(emissionElem, 'b');
 		a = this.reader.getFloat(emissionElem, 'a');
 		
+		if(	(r == 'undefined' || r < 0 || r > 1) ||
+			(g == 'undefined' || g < 0 || g > 1) ||
+			(b == 'undefined' || b < 0 || b > 1) ||
+			(a == 'undefined' || a < 0 || a > 1) )
+				return "Material -> Emission -> Missing required information or variables with wrong values";
+		
 		appearance.setEmission(r, g, b, a);
 		
 		var ambient = material_elems.getElementsByTagName('ambient');
@@ -563,6 +582,12 @@ MySceneGraph.prototype.parseMaterials = function(material) {
 		g = this.reader.getFloat(ambientElem, 'g');
 		b = this.reader.getFloat(ambientElem, 'b');
 		a = this.reader.getFloat(ambientElem, 'a');
+		
+		if(	(r == 'undefined' || r < 0 || r > 1) ||
+			(g == 'undefined' || g < 0 || g > 1) ||
+			(b == 'undefined' || b < 0 || b > 1) ||
+			(a == 'undefined' || a < 0 || a > 1) )
+				return "Material -> Ambient -> Missing required information or variables with wrong values";
 		
 		appearance.setAmbient(r, g, b, a);
 
@@ -576,6 +601,12 @@ MySceneGraph.prototype.parseMaterials = function(material) {
 		b = this.reader.getFloat(diffuseElem, 'b');
 		a = this.reader.getFloat(diffuseElem, 'a');
 		
+		if(	(r == 'undefined' || r < 0 || r > 1) ||
+			(g == 'undefined' || g < 0 || g > 1) ||
+			(b == 'undefined' || b < 0 || b > 1) ||
+			(a == 'undefined' || a < 0 || a > 1) )
+				return "Material -> Diffuse -> Missing required information or variables with wrong values";
+		
 		appearance.setDiffuse(r, g, b, a);
 								
 		var specular = material_elems.getElementsByTagName('specular');
@@ -588,6 +619,12 @@ MySceneGraph.prototype.parseMaterials = function(material) {
 		b = this.reader.getFloat(specularElem, 'b');
 		a = this.reader.getFloat(specularElem, 'a');
 		
+		if(	(r == 'undefined' || r < 0 || r > 1) ||
+			(g == 'undefined' || g < 0 || g > 1) ||
+			(b == 'undefined' || b < 0 || b > 1) ||
+			(a == 'undefined' || a < 0 || a > 1) )
+				return "Material -> Specular -> Missing required information or variables with wrong values";
+		
 		appearance.setSpecular(r, g, b, a);
 								
 		var shininess = material_elems.getElementsByTagName('shininess');
@@ -596,6 +633,9 @@ MySceneGraph.prototype.parseMaterials = function(material) {
 
 		var shininessElem = shininess[0];
 		var shininess = this.reader.getFloat(shininessElem, 'value');
+		
+		if ( shininess == 'undefined' || shininess < 0)
+			return "Materials -> Shininess-> Missing required information or variable with wrong value";
 		
 		appearance.setShininess(shininess);
 		appearance.setTextureWrap('REPEAT', 'REPEAT');
@@ -606,13 +646,7 @@ MySceneGraph.prototype.parseMaterials = function(material) {
 
 //Parses the transformations
 MySceneGraph.prototype.parseTransformations = function(transformations) {
-		
-	/*
-		Transformations
 
-		TO DO:
-		????????????????????????
-	*/
 	var nnodes = transformations.children.length;
 	
 	if (transformations == null || nnodes < 1) 
@@ -625,14 +659,19 @@ MySceneGraph.prototype.parseTransformations = function(transformations) {
 			return "Transformation error";
 
 		var id = transform_elems.attributes.getNamedItem('id').value;
+		
+		if( id == 'undefined' )
+			return "Transformation -> ID -> Missing required information.";
 	
 		// Making sure that there are no two transformations with the same id
 		if( this.transformations[id] != undefined )
-			return "Transformations -> Same id error";
+			return "Transformations -> " + this.transformations[id] + " -> Same id error";
 
 		var matrix = mat4.create();
 		this.transformations[id] = matrix;
-		//this.transformations[id] = new TransformationInfo(id);
+		
+		if(transform_elems.children.length == 0)
+			return "Transformation -> You need at least one transformation (translate, rotate or scale)";
 
 		for(var k = 0; k < transform_elems.children.length; k++){
 			var transformation = transform_elems.children[k].tagName;
@@ -641,19 +680,27 @@ MySceneGraph.prototype.parseTransformations = function(transformations) {
 				var axis, angle;
 				angle = Math.PI * this.reader.getFloat(transform_elems.children[k], 'angle') / 180;
 				axis = this.reader.getString(transform_elems.children[k], 'axis');
+				
+				if( angle == 'undefined' || axis == 'undifined')
+					return "Transformation -> Rotate -> Missing required information.";
+				else if(this.axis_length < 0)
+					console.warn("Axis length can't be negative");
+				
 				mat4.rotate(matrix, matrix, angle, axis == 'x' ? [1, 0, 0] : axis == 'y' ? [0, 1, 0] : [0, 0, 1]);
-				//this.transformations[id].addTransform(transformation, [axis, angle]);
 			}
 			else {
 				var x, y, z;
 				x = this.reader.getFloat(transform_elems.children[k], 'x');
 				y = this.reader.getFloat(transform_elems.children[k], 'y');
 				z = this.reader.getFloat(transform_elems.children[k], 'z');
+				
+				if( x == 'undefined' || y == 'undefined' || z == 'undefined')
+					return "Transformation -> " + transformation + " -> Missing required information.";
+				
 				if( transformation == 'scale' )
 					mat4.scale(matrix, matrix, [x, y, z]);
 				else
 					mat4.translate(matrix, matrix, [x, y, z]);
-				//this.transformations[id].addTransform(transformation, [x, y, z]);
 			}
 		}
 	}
@@ -661,13 +708,6 @@ MySceneGraph.prototype.parseTransformations = function(transformations) {
 
 //Parses the different primitives
 MySceneGraph.prototype.parsePrimitives = function(primitives) {
-	
-	/*
-		Primitives
-
-		TO DO:
-			everything
-	*/
 	
 	var nnodes = primitives.children.length;
 	

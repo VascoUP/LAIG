888f8b4c32ba815d9f50bc70913778a52ba528ec
saracouto1318 <saracouto1318@hotmail.com>
2016-10-23 21:48:41 +0100

    Main changed

--- TP1/WebCGF.XMLReaderExample/reader/main.js
+++ TP1/WebCGF.XMLReaderExample/reader/main.js
@@ -13,7 +13,7 @@ function getUrlVars() {
 
 serialInclude(['../lib/CGF.js', 'XMLscene.js', 'MySceneGraph.js', 
                 'ViewInfo.js', 'TextureInfo.js',
-                'MyRectangle.js', 'MyTriangle.js', 'MyCylinder.js', 'MySphere.js', 'MyTorus.js',
+                'MyRectangle.js', 'MyTriangle.js', 'MyCylinder.js', 'MySphere.js',
                 'Graph.js', 'MyInterface.js', 'MyTorus.js',
 
 main=function()
